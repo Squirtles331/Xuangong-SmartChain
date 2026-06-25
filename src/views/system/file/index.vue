b8976e94fa9b3da47e3511dc947a658b5fc90f69
玄工智链 <dev@xuanlian.com>
2026-06-25 09:52:43 +0800

    fix: 批量补全9个页面搜索/弹窗/删除功能
    - MDM设备台账/工作中心/模具: 搜索+弹窗
    - 库存盘点: 搜索
    - 销售订单: 弹窗
    - IoT采集配置: 搜索+弹窗
    - 系统参数: 删除
    - 文件管理: 搜索
    - 能耗明细: 搜索+弹窗

--- src/views/system/file/index.vue
+++ src/views/system/file/index.vue
@@ -1,5 +1,6 @@
 <template>
   <gi-page-layout :bordered="true">
+    <template #header><gi-form ref="sf" v-model="s" :columns="sc" search @search="hs" @reset="hr" /></template>
     <template #tool>
       <el-upload :auto-upload="false" :show-file-list="false" @change="handleUpload">
         <gi-button type="add">上传文件</gi-button>
